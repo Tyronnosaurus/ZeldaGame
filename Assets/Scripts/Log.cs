3cfd4bf87fa92fbba7f816c86fbe053a56e22669
Tyronnosaurus <Tyronnosaurus@protonmail.com>
2022-03-15 23:02:41 +0100

    Tutorial part 21

--- Assets/Scripts/Log.cs
+++ Assets/Scripts/Log.cs
@@ -8,12 +8,14 @@ public class Log : Enemy
     public float chaseRadius;
     public float attackRadius;
     public Transform homePosition;
+    private Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
         currentState = EnemyState.idle;
         target = GameObject.FindWithTag("Player").transform;    //
+        anim = GetComponent<Animator>();
     }
 
 
@@ -27,12 +29,45 @@ public class Log : Enemy
     {
         float distance = Vector3.Distance(target.position, transform.position);
 
-        if ((distance <= chaseRadius) && (distance > attackRadius))
+        if (distance <= chaseRadius)
         {
-            if (currentState == EnemyState.idle) ChangeState(EnemyState.walk);  // Player gets close -> Awake
-            if (currentState == EnemyState.walk) transform.position = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);  // If awoken, chase player
+            if (currentState == EnemyState.idle)
+            {
+                ChangeState(EnemyState.walk);  // Player gets close -> Awake
+                anim.SetBool("wakeUp", true);
+            }
+
+            if ((distance > attackRadius) && (currentState == EnemyState.walk)) {
+                transform.position = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);  // If awake, chase player
+                changeAnimationOrientation( target.position - transform.position );       // Used to choose which walking animation to use
+            }
         }
-	}
+
+        if (distance > chaseRadius)
+        {
+            ChangeState(EnemyState.idle);
+            anim.SetBool("wakeUp", false); // Start GoToSleep animation
+        }
+
+    }
+
+
+    // Compute which direction is a vector pointing to (by dividing the 2D into 4 quadrants with two diagonals)
+    private void changeAnimationOrientation(Vector2 dir)
+	{                                                                     //                \ U /
+		if      (dir.x >=  Mathf.Abs(dir.y))  animSetFloatsXY( 1 , 0);    // RIGHT           \|/
+		else if (dir.x <= -Mathf.Abs(dir.y))  animSetFloatsXY(-1 , 0);    // LEFT         L --+-- R
+		else if (dir.y >=  Mathf.Abs(dir.x))  animSetFloatsXY( 0 , 1);    // UP              /|\
+		else if (dir.y <=  Mathf.Abs(dir.x))  animSetFloatsXY( 0 ,-1);    // DOWN           / D \
+    }
+
+    // Update animator variables so that blend tree uses the correct animation
+    private void animSetFloatsXY(float x, float y)
+	{
+        anim.SetFloat("movX", x);
+        anim.SetFloat("movY", y);
+    }
+
 
 
 
